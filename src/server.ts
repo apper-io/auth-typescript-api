2c2a82a395251d415c989f85b39277bb1b16d649
Adrian <adrianmarrero18@gmail.com>
2023-09-13 17:32:48 -0400

    fixes middleware

--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,13 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
+// import dotenv from 'dotenv';
 
 import router from './router';
-// import mongoose from 'mongoose';
+import mongoose from 'mongoose';
 
 const app = express();
+// const dotenvConfig = dotenv.config();
 
 app.use(cors({
     credentials: true,
@@ -19,15 +21,14 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 
 const server = http.createServer(app);
+// const MONGO_URL = process.env.MONGO_DB;
 
 server.listen(8000, () => {
-    console.log('Server running on http://localhost:8080/');
+    console.log(`Server running on localhost:8000`);
 });
 
-const MONGO_URL = ''; // DB URI
-
-// mongoose.Promise = Promise;
-// mongoose.connect(MONGO_URL);
-// mongoose.connection.on('error', (error: Error) => console.log(error));
+mongoose.Promise = Promise;
+mongoose.connect('mongodb+srv://mongodb:mongodb@cluster0.8zi6ubj.mongodb.net/?retryWrites=true&w=majority');
+mongoose.connection.on('error', (error: Error) => console.log(error));
 
 app.use('/', router());
